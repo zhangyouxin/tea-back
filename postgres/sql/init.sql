7a5a62887d02edee1a37f9a4c22c54c4be6d396e
zhangyouxin <zhangyouxingg.126.com>
2019-09-02 14:36:35 +0800

    initial

---
+++ postgres/sql/init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS devices;
+
+create table users (id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL, password VARCHAR(128) NOT NULL, super smallint NOT NULL DEFAULT 0);
+CREATE UNIQUE INDEX users_username ON users (username);
+
+insert into users(username, password, super) values('admin', 'PBKDF2$sha256$901$qy3SCiU4YncLaVLv$uE/2grmw1lxKm4CsbrqcL6ObQKLXHZxk', 1);
+insert into users(username, password) values('jin', 'PBKDF2$sha256$901$qy3SCiU4YncLaVLv$uE/2grmw1lxKm4CsbrqcL6ObQKLXHZxk');
