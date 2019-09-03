4db346a3a103543f3034cf0302bbc40aed2e3dda
zhangyouxin <zhangyouxingg.126.com>
2019-09-03 14:50:17 +0800

    fix init sql

--- postgres/sql/init.sql
+++ postgres/sql/init.sql
@@ -2,12 +2,10 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS devices;
 DROP TABLE IF EXISTS messages;
 
+create table messages (id SERIAL PRIMARY KEY, sub_topic VARCHAR(25) NOT NULL, device_no VARCHAR(25) NOT NULL, message VARCHAR(128) NOT NULL DEFAULT 0);
 create table users (id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL, password VARCHAR(128) NOT NULL, super smallint NOT NULL DEFAULT 0);
 CREATE UNIQUE INDEX users_username ON users (username);
 
 insert into users(username, password, super) values('admin', 'PBKDF2$sha256$901$qy3SCiU4YncLaVLv$uE/2grmw1lxKm4CsbrqcL6ObQKLXHZxk', 1);
 insert into users(username, password) values('jin', 'PBKDF2$sha256$901$qy3SCiU4YncLaVLv$uE/2grmw1lxKm4CsbrqcL6ObQKLXHZxk');
-
-
-create table messages (id SERIAL PRIMARY KEY, sub_topic VARCHAR(25) NOT NULL, device_no VARCHAR(25) NOT NULL, message VARCHAR(128) NOT NULL DEFAULT 0);
-insert into messages(sub_topic, device_no, message) values('sub', '123', 'hello');
+insert into messages (sub_topic, device_no, message) values('sub', '123', 'hello');
