b7ec5ee83874aa2b53d49d5187c86d95f8508338
zhangyouxin <zhangyouxingg.126.com>
2019-09-03 13:50:50 +0800

    add database in py

--- src/subscribe.py
+++ src/subscribe.py
@@ -2,10 +2,33 @@ import paho.mqtt.client as mqtt
 import time
 import logging
 import os
+import re
+import psycopg2
+from enum import Enum, unique
 logging.basicConfig(filename= './log/app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 HOST = 'localhost'
 PORT = 1883
+
+@unique
+class Topic(Enum):
+    CL_Up = r'(stds\/up\/CL\/)(\d+)'            #采集数据
+    CL_Offline = r'(stds\/offline\/CL\/)(\d+)'  #设备离线
+
+    SYS_Up = r'(stds\/up\/sys\/)(\d+)' 
+    SYS_Down = r'(stds\/down\/sys\/)(\d+)'
+    SYS_Offline = r'(stds\/offline\/sys\/)(\d+)'
+
+    CT_Up = r'(stds\/up\/CT\/)(\d+)'   
+    CT_Down_Switch = r'(stds\/down\/CT\/)(\d+)\/(switch)' 
+    CT_Down_Loop = r'(stds\/down\/CT\/)(\d+)\/(loop)' 
+    CT_Offline = r'(stds\/offline\/CT\/)(\d+)' 
+
+topReg = r'stds\/(.*?)(\d+)'
+
+conn = psycopg2.connect(database="moquitto", user="root",
+                        password="dev", host="localhost", port="5432")
+logging.warning('Opened database successfully')
 
 def client_loop():
     client_id = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
@@ -18,11 +41,26 @@ def client_loop():
 
 def on_connect(client, userdata, flags, rc):
     logging.warning('Connected with result code ' + str(rc))
-    client.subscribe('/topic1')
+    client.subscribe("#")
 
 def on_message(client, userdata, msg):
-    logging.warning(msg.topic + ' ' + msg.payload.decode('utf-8'))
-    print(msg.topic + ' ' + msg.payload.decode('utf-8'))
+    logging.warning("[Info] Message received: " + msg.topic + " " + str(msg.payload))
+    topic = msg.topic 
+    res = re.search(topReg, topic)
+    if res == None:
+        logging.warning("[Debug] No Topic extracted for: " + topic)
+    else:
+        subTopic = topic.group(0)
+        deviceNo = topic.group(1)
+        logging.warning("[Debug] extracted subTopic :", subTopic , 'deviceNo: ', deviceNo, 'msg: ', str(msg.payload))
+        save_to_database(subTopic, deviceNo, str(msg.payload))
+
+def save_to_database(subTopic, deviceNo, msg):
+    sqlTemp = "insert into message(sub_topic, device_no, message) values ('{0}', '{1}', '{2}')"
+    sql = sqlTemp.format(subTopic, deviceNo, msg)
+    logging.warning(sql)
+    cur.execute(sql)
+    conn.commit()
 
 if __name__ == '__main__':
     client_loop()
